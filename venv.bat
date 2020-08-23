afd390991b7df54e281fd89e66fad2a87f1d7a68
Umberto <31143638+linuxiamo@users.noreply.github.com>
2020-08-24 01:33:55 +0200

    Added gitignore and requirements for a python virtual environment

---
+++ venv.bat
@@ -0,0 +1,6 @@
+:: This code is a shortcut to activate the 'venv' virtual environment you should create with Python and virtualenv
+:: To create the virtual environment run:
+::
+:: pip install virtualenv
+:: virtualenv venv
+venv\Scripts\activate
